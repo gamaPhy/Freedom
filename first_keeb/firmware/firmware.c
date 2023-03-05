9cfb93847351bdfa3661f45450906d26626ce69e
Thailer Lietz <thailer.s.lietz@gmail.com>
2023-03-05 07:52:57 -0600

    adds saving settings and changing settings thru VIA

--- first_keeb/firmware/firmware.c
+++ first_keeb/firmware/firmware.c
@@ -2,11 +2,27 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #include "firmware.h"
 
-int actuation_point_mm = 10;
 bool changing_actuation_settings = false;
 bool calibrating_sensors = false;
-bool rapid_trigger_on = true;
-sensor_bounds_t matrix_sensor_bounds[] = { {500, 1500}, {500, 1500}, {500, 1500} };
+kb_config_t kb_config;
+
+void eeconfig_init_kb(void) {
+    kb_config.rapid_trigger = true;
+    kb_config.actuation_point_mm = 20;
+    kb_config.release_point_mm = 16;
+    for (int col = 0; col < MATRIX_COLS; col++) {
+        kb_config.matrix_sensor_bounds[col].min = 0;
+        kb_config.matrix_sensor_bounds[col].max = 3000;
+    }
+    eeconfig_update_kb_datablock(&kb_config);
+}
+
+void keyboard_post_init_kb(void) {
+    eeconfig_read_kb_datablock(&kb_config);
+    debug_enable = true;
+    debug_matrix = true;
+    debug_keyboard = true;
+}
 
 bool process_record_kb(uint16_t keycode, keyrecord_t* record) {
     if (!process_record_user(keycode, record)) {
@@ -19,32 +35,34 @@ bool process_record_kb(uint16_t keycode, keyrecord_t* record) {
             // this will disable analog keys while calibrating
             calibrating_sensors = true;
             for (int col = 0; col < MATRIX_COLS; col++) {
-                matrix_sensor_bounds[col].min = (1 << ADC_RESOLUTION) - 1;
-                matrix_sensor_bounds[col].max = 0;
+                kb_config.matrix_sensor_bounds[col].min = -1;
+                kb_config.matrix_sensor_bounds[col].max = 0;
             }
         } else {
+            eeconfig_update_kb_datablock(&kb_config);
             calibrating_sensors = false;
         }
         return false;
     case KC_TOGGLE_RAPID_TRIGGER:
         if (record->event.pressed) {
-            rapid_trigger_on = !rapid_trigger_on;
+            kb_config.rapid_trigger = !kb_config.rapid_trigger;
+            eeconfig_update_kb_datablock(&kb_config);
         }
         return false;
     case KC_ACTUATION_DEC:
-        --actuation_point_mm;
+        if (kb_config.actuation_point_mm > 1) {
+            --kb_config.actuation_point_mm;
+            eeconfig_update_kb_datablock(&kb_config);
+        }
         return false;
     case KC_ACTUATION_INC:
-        ++actuation_point_mm;
+        if (kb_config.actuation_point_mm < 40){
+            ++kb_config.actuation_point_mm;
+            eeconfig_update_kb_datablock(&kb_config);
+        }
         return false;
     }
 
     return true;
 }
 
-void keyboard_post_init_user() {
-    debug_enable = true;
-    debug_matrix = true;
-    debug_keyboard = true;
-    // debug_mouse=true;
-}
